3e3800af8a94d191cfe565fdc254570d1087b8b3
Alysson Zanchi <alyssonzanchi@gmail.com>
2025-05-27 17:50:31 -0300

    feat(quiz): implementar comando de quiz com type guard para compatibilidade de canal

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,10 +1,12 @@
 import { CommandHandler } from "../types/CommandHandler";
 import { piada } from "./piada";
 import { ping } from "./ping";
+import quiz from "./quiz";
 import { roleta } from "./roleta";
 
 export const commands: Record<string, CommandHandler> = {
   "!piada": piada,
   "!ping": ping,
   "!roleta": roleta,
+  "!quiz": quiz,
 };
